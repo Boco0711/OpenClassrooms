3a05c7c35547de4b643503c43816f1f8a90f8caf
boco0711 <s.lep.0711@gmail.com>
2020-01-21 21:46:20 +0100

    Ajout de la méthode qui permet de cliquer sur le bouton d'ajout de voisin dans l'activité principale
    En commentaire car pas notre travail

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourActivity.java
@@ -1,15 +1,19 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.openclassrooms.entrevoisins.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ListNeighbourActivity extends AppCompatActivity {
 
@@ -20,6 +24,8 @@ public class ListNeighbourActivity extends AppCompatActivity {
     Toolbar mToolbar;
     @BindView(R.id.container)
     ViewPager mViewPager;
+    @BindView(R.id.addButton)
+    FloatingActionButton mButton;
 
     ListNeighbourPagerAdapter mPagerAdapter;
 
@@ -35,4 +41,13 @@ public class ListNeighbourActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
         mTabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
     }
+
+    /*
+    Ici la méthode qui rend le bouton Clickable, retiré pour linstant car pas d'utilité et de consigne ( Et supposé être déja présenté par le prédécesseur )
+    @OnClick(R.id.addButton)
+    public void onClick(View view) {
+        Snackbar.make(view, "Utilisateur Ajouté", Snackbar.LENGTH_LONG)
+                .setAction("", null)
+                .show();
+    }*/
 }
