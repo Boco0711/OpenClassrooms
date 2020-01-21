fb0028e2c86e028fdab49c0a8aa7dc8f68ac1097
boco0711 <s.lep.0711@gmail.com>
2020-01-21 22:58:44 +0100

    Modification des champs par défaut pour les faux voisin

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourGenerator.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/service/DummyNeighbourGenerator.java
@@ -9,18 +9,31 @@ import java.util.List;
 public abstract class DummyNeighbourGenerator {
 
     public static List<Neighbour> DUMMY_NEIGHBOURS = Arrays.asList(
-            new Neighbour(1, "Caroline", "http://i.pravatar.cc/150?u=a042581f4e29026704d", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(2, "Jack", "http://i.pravatar.cc/150?u=a042581f4e29026704e", "MIMIMIMIMIMIMIMIMI", "0595959596", "DEDOUTKCJHNHECEBgh", "BLIERTRERTDFEZDD"),
-            new Neighbour(3, "Chloé", "http://i.pravatar.cc/150?u=a042581f4e29026704f", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(4, "Vincent", "http://i.pravatar.cc/150?u=a042581f4e29026704a", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(5, "Elodie", "http://i.pravatar.cc/150?u=a042581f4e29026704b", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(6, "Sylvain", "http://i.pravatar.cc/150?u=a042581f4e29026704c", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(7, "Laetitia", "http://i.pravatar.cc/150?u=a042581f4e29026703d", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(8, "Dan", "http://i.pravatar.cc/150?u=a042581f4e29026703b", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(9, "Joseph", "http://i.pravatar.cc/150?u=a042581f4e29026704d", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(10, "Emma", "http://i.pravatar.cc/150?u=a042581f4e29026706d", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(11, "Patrick", "http://i.pravatar.cc/150?u=a042581f4e29026702d", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla"),
-            new Neighbour(12, "Ludovic", "http://i.pravatar.cc/150?u=a042581f3e39026702d", "bouyabouya", "060606060606", "azertyuioplkhqsdfgh", "blablablablablabla")
+            new Neighbour(1, "Caroline", "http://i.pravatar.cc/150?u=a042581f4e29026704d", "Saint pierre du mont à 5km", "+33 6 86 57 90 14", "www.facebook.fr/caroline",
+                    "Bonjour! je souhaiterais faire de la marche nordique." +
+                            "Pas initié, je recherche une ou plusieur personnes susceptible de m'accompagner! J'aime les jeux de cartes tels la belote et le tarot...", false),
+            new Neighbour(2, "Jack", "http://i.pravatar.cc/150?u=a042581f4e29026704e", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(3, "Chloé", "http://i.pravatar.cc/150?u=a042581f4e29026704f", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(4, "Vincent", "http://i.pravatar.cc/150?u=a042581f4e29026704a", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(5, "Elodie", "http://i.pravatar.cc/150?u=a042581f4e29026704b", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(6, "Sylvain", "http://i.pravatar.cc/150?u=a042581f4e29026704c", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(7, "Laetitia", "http://i.pravatar.cc/150?u=a042581f4e29026703d", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(8, "Dan", "http://i.pravatar.cc/150?u=a042581f4e29026703b", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(9, "Joseph", "http://i.pravatar.cc/150?u=a042581f4e29026704d", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(10, "Emma", "http://i.pravatar.cc/150?u=a042581f4e29026706d", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(11, "Patrick", "http://i.pravatar.cc/150?u=a042581f4e29026702d", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false),
+            new Neighbour(12, "Ludovic", "http://i.pravatar.cc/150?u=a042581f3e39026702d", "Une adresse ou un emplacement",
+                    "Un numéro de télephone", "Une adresse vers réseau social", "Une description", false)
     );
 
     static List<Neighbour> generateNeighbours() {
